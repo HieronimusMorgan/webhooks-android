ce501ec8ef089e2edb71261d3f622254d6952bdf
Hieronimus Fredy Morgan <hieronimus.fredy@nexsoft.co.id>
2024-08-06 09:56:43 +0700

    Add Send Log With TAG

--- webhook/src/main/java/com/morg/webhook/configuration/SendLog.java
+++ webhook/src/main/java/com/morg/webhook/configuration/SendLog.java
@@ -43,13 +43,63 @@ public class SendLog {
 
     public void sendLogVerbose(String message) {
         new WebhooksConfiguration(context, webhooksUrl)
-                .addField(new Fields("Log Verbose", message))
+                .setTitle("Log Verbose")
+                .setDescription(message)
                 .sendLog();
     }
 
     public void sendLogAssert(String message) {
         new WebhooksConfiguration(context, webhooksUrl)
-                .addField(new Fields("Log Assert", message))
+                .setTitle("Log Assert")
+                .setDescription(message)
+                .sendLog();
+    }
+
+    public void sendLogError(String tag, Throwable throwable) {
+        new WebhooksConfiguration(context, webhooksUrl)
+                .setTitle("Log Error")
+                .setDescription(throwable.getMessage())
+                .addField(new Fields("TAG", tag))
+                .sendLog();
+    }
+
+    public void sendLogInfo(String tag, String message) {
+        new WebhooksConfiguration(context, webhooksUrl)
+                .setTitle("Log Info")
+                .setDescription(message)
+                .addField(new Fields("TAG", tag))
+                .sendLog();
+    }
+
+    public void sendLogWarning(String tag, String message) {
+        new WebhooksConfiguration(context, webhooksUrl)
+                .setTitle("Log Warning")
+                .setDescription(message)
+                .addField(new Fields("TAG", tag))
+                .sendLog();
+    }
+
+    public void sendLogDebug(String tag, String message) {
+        new WebhooksConfiguration(context, webhooksUrl)
+                .setTitle("Log Debug")
+                .setDescription(message)
+                .addField(new Fields("TAG", tag))
+                .sendLog();
+    }
+
+    public void sendLogVerbose(String tag, String message) {
+        new WebhooksConfiguration(context, webhooksUrl)
+                .setTitle("Log Verbose")
+                .setDescription(message)
+                .addField(new Fields("TAG", tag))
+                .sendLog();
+    }
+
+    public void sendLogAssert(String tag, String message) {
+        new WebhooksConfiguration(context, webhooksUrl)
+                .setTitle("Log Assert")
+                .setDescription(message)
+                .addField(new Fields("TAG", tag))
                 .sendLog();
     }
 }
